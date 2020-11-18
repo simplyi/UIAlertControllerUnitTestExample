d98d078ec103b626dfa09f3417f36a07b88d41ce
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-11-18 18:37:29 -0500

    Initial Commit

---
+++ UIAlertControllerUnitTestExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIAlertControllerUnitTestExample
+//
+//  Created by Sergey Kargopolov on 2020-11-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
